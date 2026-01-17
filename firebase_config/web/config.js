600b116f6107730aa256e1dbee27d7d7813c9830
Nathanlovaniaina <142962456+Nathanlovaniaina@users.noreply.github.com>
2026-01-17 13:44:17 +0300

    Add Firebase web config and authentication test files
    Introduces Firebase configuration for web, an HTML page to test email/password authentication, and a REST Client HTTP file for manual API testing. These additions facilitate local and manual verification of Firebase Authentication setup.

---
+++ firebase_config/web/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-bnLys5qhH2dgrLJw6wkpQ2uetHni1Iw",
+  authDomain: "road-report-auth-projets5.firebaseapp.com",
+  projectId: "road-report-auth-projets5",
+  storageBucket: "road-report-auth-projets5.firebasestorage.app",
+  messagingSenderId: "301784125105",
+  appId: "1:301784125105:web:d0e2da62f2353da9f11c90"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
